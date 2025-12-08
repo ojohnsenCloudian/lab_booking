05c20c87e2e6bde2f4e6eec2314af2f327bb2d7c
ojohnsenCloudian <62557529+ojohnsenCloudian@users.noreply.github.com>
2025-12-08 21:11:27 +0100

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,10 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./prisma"
 import bcrypt from "bcryptjs"
 import type { User } from "@prisma/client"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [
     Credentials({
       credentials: {
